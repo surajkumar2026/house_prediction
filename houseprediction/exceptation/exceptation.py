1fddfbd9eead3f7fa5b31a61c8649ee8f2b80456
Surajkumar9798 <me22b1073@iiitdm.ac.in>
2025-04-05 22:33:13 +0530

    exception and logging done

--- houseprediction/exceptation/exceptation.py
+++ houseprediction/exceptation/exceptation.py
@@ -16,7 +16,7 @@ class housepredException(Exception):
         
     def __str__(self):
         return "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
-            self.file_name,self.lineno,str(self.error_message)
+            self.file_name,self.lineno ,str(self.error_message)
         )
          
 if __name__=='__main__':
